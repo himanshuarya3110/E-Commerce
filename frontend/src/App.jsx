2bbbcdf50073af630b09b9038d99671628fc99f5
himanshuarya3110 <himanshuarya3110@gmail.com>
2024-06-11 15:22:54 +0530

    product and productlist add with filters

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 
 function App() {
-  return <Home />;
+  return (
+    // <Home />
+    // <ProductList />
+    <Product />
+  );
 }
 
 export default App;
