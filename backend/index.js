fbb53644eac7ce6c27d4fc1b58ac1c142d177dbc
himanshuarya3110 <himanshuarya3110@gmail.com>
2024-06-12 02:49:12 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 const cors = require("cors");
 
 dotenv.config();
@@ -19,7 +22,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
