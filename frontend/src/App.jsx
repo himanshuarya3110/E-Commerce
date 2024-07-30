8d740dfb9ae9843703bafb34fd653ae210219df8
himanshuarya3110 <himanshuarya3110@gmail.com>
2024-07-30 22:10:28 +0530

    Add Routing and backend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,34 @@ import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
 function App() {
+  const user = true;
   return (
-    // <Home />
-    // <ProductList />
-    // <Product />
-    // <Register />
-    // <Login />
-    <Cart />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={user ? <Navigate replace to="/" /> : <Register />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -3,13 +3,14 @@ import { popularProducts } from "../data";
 import Product from "./Product";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  console.log(cat, filters, sort);
   return (
     <Container>
       {popularProducts.map((item) => (
